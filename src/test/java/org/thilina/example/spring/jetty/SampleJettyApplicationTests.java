41646e95b2393911171746ec9a67758113307dc6
Thilina Piyasundara <thilinapiy@gmail.com>
2018-01-04 09:26:52 +0530

    Update source

--- src/test/java/org/thilina/example/spring/jetty/SampleJettyApplicationTests.java
+++ src/test/java/org/thilina/example/spring/jetty/SampleJettyApplicationTests.java
@@ -50,7 +50,7 @@ public class SampleJettyApplicationTests {
 		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
 				"http://localhost:" + this.port, String.class);
 		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		assertEquals("Hello World", entity.getBody());
+		assertEquals("Hello World !!!", entity.getBody());
 	}
 
 }
